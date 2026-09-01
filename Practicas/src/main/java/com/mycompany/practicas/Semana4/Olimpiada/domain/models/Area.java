b39db78e0b9dd3cc61464d3b8e362de197a3cb60
AugustoDevZ <ruiton2025@gmail.com>
2026-09-01 13:48:40 -0500

    backup

--- Practicas/src/main/java/com/mycompany/practicas/Semana4/Olimpiada/domain/models/Area.java
+++ Practicas/src/main/java/com/mycompany/practicas/Semana4/Olimpiada/domain/models/Area.java
@@ -3,7 +3,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.practicas.Semana4.Olimpiada.domain.models;
-import com.mycompany.practicas.Semana4.Olimpiada.domain.enums.Ubicacion;
 /**
  *
  * @author AUGUSTO RODRIGUEZ
@@ -11,5 +10,13 @@ import com.mycompany.practicas.Semana4.Olimpiada.domain.enums.Ubicacion;
 public class Area {
     private String idArea;
     private String descripcion;
-    private Ubicacion localizacion;
+    private String localizacion;
+
+    public Area(String idArea, String descripcion, String localizacion) {
+        this.idArea = idArea;
+        this.descripcion = descripcion;
+        this.localizacion = localizacion;
+    }
+    
+    
 }
